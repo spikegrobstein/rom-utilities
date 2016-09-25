e7412e121d49e0e44be56aeab16856c38b71ea27
Spike Grobstein <me@spike.cx>
2016-09-25 09:34:42 -0700

    added menu choices and output to file

--- rom-curator.rb
+++ rom-curator.rb
@@ -1,5 +1,6 @@
 require 'bundler/setup'
 require 'pry'
+require 'highline'
 
 # given:
 # "./Puzzler 2000 V.43 (Aug 26) (1998) (PD).lnx"
@@ -28,7 +29,14 @@ rescue
   raise
 end
 
-listfile = ARGV.first
+listfile = ARGV[0]
+outfile = ARGV[1]
+
+unless listfile && outfile
+  puts "USAGE: rom-curator <infile> <outfile>"
+  exit 1
+end
+
 puts "Parsing: #{ listfile }"
 
 results = File.open(listfile).map { |line|
@@ -47,11 +55,44 @@ results = File.open(listfile).map { |line|
   acc
 }
 
-results.each { |key, files|
-  if files.length == 1
-    puts "Skipping #{key}"
-    next
-  end
+File.open(outfile, 'w') do |f|
+  cli = HighLine.new
 
-  puts "Choice: #{key} - #{files.length}"
-}
+  results.each { |key, files|
+    if files.length == 1
+      puts "Skipping #{key}"
+      f.puts files[0][:filename]
+      next
+    end
+
+    puts "Choice: #{key} - #{files.length}"
+    choice = cli.choose do |menu|
+      menu.prompt = "Which rom for #{ key }?"
+      menu.choice :all
+      menu.choice :none
+
+      filenames = files.map { |file|
+        file[:filename]
+      }.sort { |a,b|
+        a <=> b
+      }
+
+      filenames.each { |filename|
+        menu.choice filename
+      }
+
+      menu.default = filenames.last
+    end
+
+    if choice == :all
+      files.each do |file|
+        f.puts file[:filename]
+      end
+    elsif choice == :none
+      puts "Skipping #{key}"
+    else
+      f.puts choice
+    end
+  }
+end
+
